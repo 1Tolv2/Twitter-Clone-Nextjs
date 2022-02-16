27248a7e381107d325dd704ffe1120b252978a4a
Sofia <sofiaq1@gmail.com>
2022-02-16 20:08:00 +0100

    Added SideSection.jsx

---
+++ client/components/SideSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+outline: 2px purple solid;
+`
+export default function SideSection() {
+  return (
+    <StyledContainer>SideSection</StyledContainer>
+  )
+}
