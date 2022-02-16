0d4e09d4bbceab19cc25d3a8c424769b41755f2b
Sofia <sofiaq1@gmail.com>
2022-02-16 20:46:52 +0100

    Added basic content to Navigation bar

--- client/components/NavigationBar.jsx
+++ client/components/NavigationBar.jsx
@@ -2,10 +2,39 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledContainer = styled.div`
+display: flex;
+justify-self: end;
 outline: 2px purple solid;
+flex-flow: wrap row;
 `
+const StyledLogo = styled.div`
+
+max-width: 170px;
+height: auto;
+img {width: 100%;
+  height: auto;}
+`
+const StyledButton = styled.button`
+width: 100%;
+padding: 15px;
+border-radius: 30px;
+border-style: none;
+background-color: green;
+color: white;
+`
+
 export default function NavigationBar() {
   return (
-    <StyledContainer>NavigationBar</StyledContainer>
+    <StyledContainer>
+      <StyledLogo><img src="./Flying-cow.svg"/></StyledLogo>
+      <ul>
+      <li>Home</li>
+      <li>Explore</li>
+      <li>Notifications</li>
+      <li>Messages</li>
+      <li>Profile</li>
+      <li>More</li>
+      </ul>
+      <StyledButton>Moo</StyledButton></StyledContainer>
   )
 }
