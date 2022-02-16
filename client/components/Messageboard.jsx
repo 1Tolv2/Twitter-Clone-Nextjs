a81d966643a22c2172f2effd322466a9afd37387
Sofia <sofiaq1@gmail.com>
2022-02-16 21:05:50 +0100

    Refactored Messageboard.jsx

--- client/components/Messageboard.jsx
+++ client/components/Messageboard.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const StyledContainer = styled.div`
-max-width: 600px;
-height: 50vh;
-outline: 2px solid purple;
-
-`
 
 export default function Messageboard() {
   return (
-    <StyledContainer>Messageboard</StyledContainer>
+    <div>Messageboard</div>
   )
 }
