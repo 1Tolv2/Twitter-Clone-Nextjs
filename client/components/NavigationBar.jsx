3a9a9eb643f1adf93863ed9036aec63617b52cc1
Sofia <sofiaq1@gmail.com>
2022-02-16 20:07:50 +0100

    Added NavigationBar.jsx

---
+++ client/components/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledContainer = styled.div`
+outline: 2px purple solid;
+`
+export default function NavigationBar() {
+  return (
+    <StyledContainer>NavigationBar</StyledContainer>
+  )
+}
