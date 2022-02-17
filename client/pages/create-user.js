33184d9eea75de325de5fb2bbd29126688967939
Sofia <sofiaq1@gmail.com>
2022-02-18 00:29:27 +0100

    Added submit event so one becomes redirected

--- client/pages/create-user.js
+++ client/pages/create-user.js
@@ -1,6 +1,43 @@
-import React from "react";
-import AlternativeLayout from "../components/AlternativeLayout";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { API } from "../components/API";
+import LayoutCenterDiv from "../components/LayoutCenterDiv";
+import InputField from "../components/InputField";
+import Button from "../components/Button";
 
-export default function CreateUser({ children }) {
-  return <AlternativeLayout>LoginPage{children}</AlternativeLayout>;
+export default function CreateUser() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    const payload = { username, password };
+    console.log(API);
+    console.log(payload);
+    router.push("/login");
+  }
+  return (
+    <LayoutCenterDiv>
+      <h2>Create User</h2>
+      <form onSubmit={handleOnSubmit}>
+        <InputField
+          type="text"
+          id="username"
+          value={username}
+          setValue={setUsername}
+          placeholder="Username"
+        />
+        <InputField
+          type="text"
+          id="password"
+          value={password}
+          setValue={setPassword}
+          placeholder="Password"
+        />
+        <Button type="submit">Log in</Button>
+      </form>
+    </LayoutCenterDiv>
+  );
 }
--- client/pages/login.js
+++ client/pages/login.js
@@ -1,7 +1,42 @@
-import React from "react";
-import Layout from "../components/Layout";
-import AlternativeLayout from "../components/AlternativeLayout";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+import { API } from "../components/API";
+import LayoutCenterDiv from "../components/LayoutCenterDiv";
+import InputField from "../components/InputField";
+import Button from "../components/Button";
 
-export default function Login({ children }) {
-  return <AlternativeLayout>LoginPage{children}</AlternativeLayout>;
+export default function Login() {
+  const router = useRouter();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  function handleOnSubmit(e) {
+    e.preventDefault();
+    const payload = { username, password };
+    console.log(API);
+    console.log(payload);
+    router.push("/");
+  }
+  return (
+    <LayoutCenterDiv>
+      <h2>Login</h2>
+      <form onSubmit={handleOnSubmit}>
+        <InputField
+          type="text"
+          id="username"
+          value={username}
+          setValue={setUsername}
+          placeholder="Username"
+        />
+        <InputField
+          type="text"
+          id="password"
+          value={password}
+          setValue={setPassword}
+          placeholder="Password"
+        />
+        <Button type="submit">Log in</Button>
+      </form>
+    </LayoutCenterDiv>
+  );
 }
