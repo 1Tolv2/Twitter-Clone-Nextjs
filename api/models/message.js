1eb1c38714523a5f3fedd318fc2953819a7e7ea2
Sofia <sofiaq1@gmail.com>
2022-02-17 23:01:55 +0100

    Added attributes to schema

--- api/models/message.js
+++ api/models/message.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
   author: { type: String, required: true },
-  message: { type: String, required: true },
+  message: { type: String, required: true, maxLength: 140 },
+  date: { type: Date, default: Date.now },
 });
 
 const Message = mongoose.model("Message", messageSchema);
