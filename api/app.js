b575dc3d1de8f6d28fea1c2b5611c2006f8a0be0
Sofia <sofiaq1@gmail.com>
2022-02-19 00:08:36 +0100

    db /user has post and get methods now

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const { Message } = require("./models/message");
+const { User } = require("./models/user");
 const bodyParser = require("body-parser");
 // const indexRouter = require("./routes/index");
 
@@ -30,6 +31,28 @@ app.post("/", (req, res, next) => {
       next(err);
     } else {
       res.send("You have mooed successfully");
+    }
+  });
+});
+
+app.get("/users", async (req, res) => {
+  const data = await User.find().exec();
+  res.send(data);
+});
+
+app.post("/users", async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({
+    username,
+    password,
+  });
+  user.save((err) => {
+    if (err) {
+      console.error("ERROR:", err.errors.message.kind);
+      res.status(400).send("Unsuccessful");
+      next(err);
+    } else {
+      res.send("Welcome to the family!");
     }
   });
 });
