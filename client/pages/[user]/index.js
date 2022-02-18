3d90d3de51af84aa99fd2fc9df4dbf1a5b3341d2
Sofia <sofiaq1@gmail.com>
2022-02-19 00:07:16 +0100

    Added GSSPaths to fetch user paths

--- client/pages/[user]/index.js
+++ client/pages/[user]/index.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { API } from "../../components/API";
 import Layout from "../../components/Layout";
 
-export default function User({ children }) {
-  return <Layout>UserPage{children}</Layout>;
+export default function User({ data }) {
+  const [userMessageList, setUserMessageList] = useState(null);
+  // useEffect(() => {
+  //   setUserMessageList(data);
+  // }, [data]);
+  return <Layout>UserPage</Layout>;
+}
+
+// export async function getStaticProps() {
+//   const res = await fetch(API, {
+//     headers: { "Content-Type": "application/json" },
+//   });
+//   const data = await res.json();
+
+//   return { props: { data } };
+// }
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API}/users`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await res.json();
+
+  const pathList = data.map((user) => {
+    return {
+      params: { id: user.id.toString() },
+    };
+  });
+  return { pathList, fallback: false };
 }
