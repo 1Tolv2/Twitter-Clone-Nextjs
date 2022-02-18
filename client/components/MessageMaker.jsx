0b9ea0856dc75a754507e67686c06f4213191ccd
Sofia <sofiaq1@gmail.com>
2022-02-18 20:13:06 +0100

    Style changes made

--- client/components/MessageMaker.jsx
+++ client/components/MessageMaker.jsx
@@ -1,14 +1,22 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components';
-import InputField from './InputField';
+import {API} from "./API"
+// import InputField from './InputField';
 
 const StyledContainer = styled.div`
+display: grid;
+grid-template-columns: 100px auto;
 padding: 20px;
+img {
+  width: 70px;
+}
 `
 
 const StyledTextArea = styled.textarea`
   resize: none;
   width: 100%;
+  height: 95px;
+  font-size: 1.4em;
   border: none;
   -ms-overflow-style: none; // for Internet Explorer, Edge
   scrollbar-width: none; // for Firefox
@@ -16,8 +24,20 @@ const StyledTextArea = styled.textarea`
   &::-webkit-scrollbar {
     display: none; // for Chrome, Safari, and Opera
   }
+  &:focus {
+  outline: none;
+}
 `;
 
+const StyledButtonContainer = styled.div`
+position: relative;
+img {
+  position:absolute;
+  top: 5px;
+  left: 12px;
+  width: 30px;
+}
+`
 const StyledButton = styled.button`
 width: 60px;
 height: 40px;
@@ -27,10 +47,15 @@ background-color: #00AFB9;
 color: white;
 `
 
-export default function MessageMaker() {
+export default function MessageMaker() {    
     const [author, setAuthor] = useState("");
     const [message, setMessage] = useState("");
+    const [placeholder, setPlaceholder] = useState("")
 
+    const placeholderList = ["Lay down the moos", "Moo to your hearts desire"]
+    useEffect(() => {
+        setPlaceholder(placeholderList[Math.floor(Math.random() * 2)])
+    }, [])
     async function handleOnSubmit(e) {
         setAuthor("test")
         e.preventDefault();
@@ -45,16 +70,20 @@ export default function MessageMaker() {
 
   return (
     <StyledContainer>
+      <img src="./Stylized-Cow-Line-Art.svg"/>
     <form onSubmit={handleOnSubmit}>
-      Moo to your hearts desire
       <StyledTextArea
         type="text"
         name="message"
         value={message}
         onChange={(e) => setMessage(e.target.value)}
-        placeholder={}
+        placeholder={placeholder + "..."}
+        maxlength="140"
       />
-      <StyledButton type="submit">S</StyledButton>
+      <StyledButtonContainer>
+        <img src="./send-paper.svg"/>
+        <StyledButton type="submit"></StyledButton>
+      </StyledButtonContainer>
     </form>
   </StyledContainer>
   )
