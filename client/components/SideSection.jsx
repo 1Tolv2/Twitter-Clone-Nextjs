94ef9f7a95c86753b05ce4bd8b04f7fdd607c723
Sofia <sofiaq1@gmail.com>
2022-02-18 22:11:14 +0100

    Added basic styling to SideSection

--- client/components/SideSection.jsx
+++ client/components/SideSection.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
+import Button from "./Button"
 
 const StyledContainer = styled.div`
+display: flex;
+justify-content: center;
+align-content: flex-start;
+flex-flow: wrap row;
 justify-self: start;
 width: 100%;
-outline: 2px purple solid;
+background-color: #FDFCDC;
+border-radius: 5px;
+padding: 20px;
+text-align: center;
+button{ background-color: #F07167;}
+h2 {
+  height: fit-content;
+}
 `
+
+const StyledList = styled.ul`
+list-style-type: none;
+padding: 0;
+`
+
+
 export default function SideSection() {
   return (
-    <StyledContainer>SideSection</StyledContainer>
+    <StyledContainer><h2>Hello User!</h2>
+    <StyledList>
+    <li>Settings</li>
+    <Button>Log out</Button>
+    </StyledList></StyledContainer>
   )
 }
