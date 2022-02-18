f4a97b49739cddf30bf777aba03fb00e50050936
Sofia <sofiaq1@gmail.com>
2022-02-19 00:06:46 +0100

    Changed behaviour for more use of messageboard

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from "react";
 import Messageboard from "../components/Messageboard";
 import Layout from "../components/Layout";
 import { API } from "../components/API";
+import MessageMaker from "../components/MessageMaker";
 
 export default function Home({ data }) {
-  console.log(API);
   const [messageList, setMessageList] = useState(null);
+
   useEffect(() => {
     setMessageList(data);
-  }, []);
+  }, [data]);
   return (
     <Layout>
-      <Messageboard data={messageList} />
+      <Messageboard data={messageList}>
+        <MessageMaker />
+      </Messageboard>
     </Layout>
   );
 }
-
 export async function getStaticProps() {
   const res = await fetch(API, {
     headers: { "Content-Type": "application/json" },
