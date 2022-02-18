7ff4b8d3ecb884774a082f028d333a8c9d794e67
Sofia <sofiaq1@gmail.com>
2022-02-19 00:38:36 +0100

    Moved messageItem rendering to MessageItem.jsx

--- client/components/MessageItem.jsx
+++ client/components/MessageItem.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
 
-export default function MessageItem() {
+const StyledSection = styled.article`
+  margin: 15px 0;
+  display: grid;
+  gap: 15px;
+  grid-template-columns: 60px auto;
+`;
+const StyledImage = styled.div`
+  width: 100%;
+  height: 60px;
+  padding: 5px;
+  background-color: white;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+const StyledContainer = styled.div`
+  background-color: white;
+  border-radius: 5px;
+  padding: 10px;
+  overflow-x: hidden;
+  h3 {
+    margin: 0;
+  }
+`;
+
+export default function MessageItem({data}) {
   return (
-    <div>MessageItem</div>
-  )
+    <StyledSection>
+      <Link href={`/${data.author}`}>
+        <StyledImage>
+          <img src="profile-svgrepo-com.svg" height="50px" />
+        </StyledImage>
+      </Link>
+      <StyledContainer>
+        <h3>
+          <Link href={`/${data.author}`}>{data.author}</Link>
+        </h3>
+        <hr />
+        <p>{data.message}</p>
+      </StyledContainer>
+    </StyledSection>
+  );
 }
--- client/components/Messageboard.jsx
+++ client/components/Messageboard.jsx
@@ -1,34 +1,9 @@
 import React from "react";
-import Link from "next/link";
-import MessageMaker from "./MessageMaker";
 import styled from "styled-components";
+import MessageItem from "./MessageItem";
 
 const StyledWrapper = styled.div`
   width: 100%;
-`;
-const StyledSection = styled.article`
-  margin: 15px 0;
-  display: grid;
-  gap: 15px;
-  grid-template-columns: 60px auto;
-`;
-
-const StyledImage = styled.div`
-  width: 100%;
-  height: 60px;
-  padding: 5px;
-  background-color: white;
-  border-radius: 5px;
-  cursor: pointer;
-`;
-const StyledContainer = styled.div`
-  background-color: white;
-  border-radius: 5px;
-  padding: 10px;
-  overflow-x: hidden;
-  h3 {
-    margin: 0;
-  }
 `;
 
 export default function Messageboard({ data, children }) {
@@ -38,21 +13,7 @@ export default function Messageboard({ data, children }) {
       <StyledWrapper>
         {data &&
           data.map((item) => {
-            return (
-              <StyledSection key={item._id}>
-                <Link href={`/${item.author}`}>
-                  <StyledImage>
-                    <img src="profile-svgrepo-com.svg" height="50px" />
-                  </StyledImage>
-                </Link>
-                <StyledContainer>
-                  <h3>
-                    <Link href={`/${item.author}`}>{item.author}</Link>
-                  </h3>
-                  <hr />
-                  <p>{item.message}</p>
-                </StyledContainer>
-              </StyledSection>
+            return ( <><MessageItem key={item._id} data={item}/></>
             );
           })}
       </StyledWrapper>
