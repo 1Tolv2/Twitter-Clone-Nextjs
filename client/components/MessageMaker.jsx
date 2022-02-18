b995577f3fe687b88fb4953612958257f77da520
Sofia <sofiaq1@gmail.com>
2022-02-18 22:48:21 +0100

    Added message length indicator

--- client/components/MessageMaker.jsx
+++ client/components/MessageMaker.jsx
@@ -4,23 +4,29 @@ import { API } from "./API";
 // import InputField from './InputField';
 
 const StyledContainer = styled.div`
+  position: sticky;
+  top: 0;
+  box-shadow: 2px 2px 15px #007599;
   display: grid;
   grid-template-columns: 100px auto;
-  height: 180px;
+  height: fit-content;
   background-color: white;
   border-radius: 5px;
   padding: 20px;
   img {
     width: 70px;
   }
+  span {
+    &.redText {
+      color: red;
+    }
+  }
 `;
-
-
 
 const StyledTextArea = styled.textarea`
   resize: none;
   width: 100%;
-  height: 100px;
+  height: 115px;
   font-size: 1.4em;
   border: none;
   -ms-overflow-style: none; // for Internet Explorer, Edge
@@ -35,7 +41,7 @@ const StyledTextArea = styled.textarea`
 `;
 
 const StyledButtonContainer = styled.div`
-position: relative;
+  position: relative;
   float: right;
   img {
     position: absolute;
@@ -56,38 +62,48 @@ const StyledButton = styled.button`
 `;
 
 export default function MessageMaker() {
-  const [author, setAuthor] = useState("Test");
+  const [author, setAuthor] = useState("");
   const [message, setMessage] = useState("");
   const [placeholder, setPlaceholder] = useState("");
+  const [messageLength, setMessageLength] = useState(0);
 
   const placeholderList = ["Lay down the moos", "Moo to your hearts desire"];
   useEffect(() => {
     setPlaceholder(placeholderList[Math.floor(Math.random() * 2)]);
   }, []);
   async function handleOnSubmit(e) {
-    setAuthor("test");
     e.preventDefault();
     console.log("Submit handled");
     const res = await fetch(API, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ author, message }),
+      body: JSON.stringify({ author: "Test", message }),
     });
     console.log(res);
+    setMessage(""); //Empties the message field after submitting the message
   }
+
+  function handleOnClick() {}
 
   return (
     <StyledContainer>
       <img src="./Stylized-Cow-Line-Art.svg" />
       <form onSubmit={handleOnSubmit}>
         <StyledTextArea
+          onClick={handleOnClick}
           type="text"
           name="message"
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
+          onChange={(e) => {
+            setMessage(e.target.value);
+            setMessageLength(e.target.value.length);
+          }}
           placeholder={placeholder + "..."}
           maxlength="140"
         />
+        <span className={messageLength > 140 ? "redText" : null}>
+          {messageLength}
+        </span>
         <StyledButtonContainer onClick={handleOnSubmit}>
           <img src="./send-paper.svg" />
           <StyledButton></StyledButton>
