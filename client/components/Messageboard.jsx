41517a3540580342a83c8d74374fb0b378ac71e7
Sofia <sofiaq1@gmail.com>
2022-02-18 19:17:45 +0100

    Moved code to MessageMaker

--- client/components/Messageboard.jsx
+++ client/components/Messageboard.jsx
@@ -1,50 +1,39 @@
-import React, { useState } from "react";
-import Link from "next/link"
+import React from "react";
+import Link from "next/link";
+import styled from "styled-components";
+import MessageMaker from "./MessageMaker";
 
-export default function Messageboard({data}) {
-  const [author, setAuthor] = useState("");
-  const [message, setMessage] = useState("")
- 
-  async function handleOnSubmit(e) {
-    e.preventDefault();
+const StyledContainer = styled.div`
 
-    const res = await fetch("http://localhost:9000/", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({author, message})
-    })
-    console.log(res)
+`;
+
+const StyledTextArea = styled.textarea`
+  resize: none;
+  width: 100%;
+  -ms-overflow-style: none; // for Internet Explorer, Edge
+  scrollbar-width: none; // for Firefox
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none; // for Chrome, Safari, and Opera
   }
+`;
+
+export default function Messageboard({ data }) {
 
   return (
     <div>
-      <form onSubmit={handleOnSubmit}>
-        Moo to your hearts desire
-        <br />
-        Author:
-        <br />
-        <input
-          type="text"
-          name="author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <br />
-        Message:
-        <br />
-        <textarea
-          type="text"
-          name="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+<MessageMaker/>
       <div>
-        {data && data.map((item) => {
-return <article key={item._id}><Link href={`/${item.author}`} >{item.author}</Link> - {item.message}</article>
-      })}</div>
+        {data &&
+          data.map((item) => {
+            return (
+              <article key={item._id}>
+                <Link href={`/${item.author}`}>{item.author}</Link> -{" "}
+                {item.message}
+              </article>
+            );
+          })}
+      </div>
     </div>
   );
 }
