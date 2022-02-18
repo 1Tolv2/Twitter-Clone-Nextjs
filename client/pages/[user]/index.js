098ff4aba9ab203fd93db3f4a436fc0b7f045cff
Sofia <sofiaq1@gmail.com>
2022-02-19 00:32:04 +0100

    user pages now filters messages on queryparams

--- client/pages/[user]/index.js
+++ client/pages/[user]/index.js
@@ -1,13 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import { API } from "../../components/API";
 import Layout from "../../components/Layout";
+import Messageboard from "../../components/Messageboard";
 
-export default function User({ data }) {
+export default function User() {
+  const router = useRouter();
   const [userMessageList, setUserMessageList] = useState(null);
-  // useEffect(() => {
-  //   setUserMessageList(data);
-  // }, [data]);
-  return <Layout>UserPage</Layout>;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(API, {
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      console.log(data);
+      const user = router.query;
+
+      console.log(user.user);
+      const messageList = data.filter((message) => {
+        return message.author === user.user;
+      });
+
+      return setUserMessageList(messageList);
+    };
+
+    fetchData();
+  }, [router.query]);
+  return (
+    <Layout>
+      <Messageboard data={userMessageList}>UserPage</Messageboard>
+    </Layout>
+  );
 }
 
 // export async function getStaticProps() {
@@ -19,16 +43,16 @@ export default function User({ data }) {
 //   return { props: { data } };
 // }
 
-export async function getStaticPaths() {
-  const res = await fetch(`${API}/users`, {
-    headers: { "Content-Type": "application/json" },
-  });
-  const data = await res.json();
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API}/users`, {
+//     headers: { "Content-Type": "application/json" },
+//   });
+//   const data = await res.json();
 
-  const pathList = data.map((user) => {
-    return {
-      params: { id: user.id.toString() },
-    };
-  });
-  return { pathList, fallback: false };
-}
+//   const pathList = data.map((user) => {
+//     return {
+//       params: { id: user.id.toString() },
+//     };
+//   });
+//   return { pathList, fallback: false };
+// }
