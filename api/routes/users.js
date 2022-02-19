6fc16536ca57dbd7d485771d0961a8e0f6e992b7
Sofia <sofiaq1@gmail.com>
2022-02-19 15:04:55 +0100

    GET and POST function for users

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,27 @@
 const express = require("express");
-const { Message } = require("../models/message");
+const { User } = require("../models/user");
 const router = express.Router();
+
+router.get("/users", async (req, res) => {
+  const data = await User.find().exec();
+  res.send(data);
+});
+
+router.post("/users", async (req, res) => {
+  const { username, password } = req.body;
+  const user = new User({
+    username,
+    password,
+  });
+  user.save((err) => {
+    if (err) {
+      console.error("ERROR:", err.errors.message.kind);
+      res.status(400).send({ errorMessage: "Unsuccessful" });
+      next(err);
+    } else {
+      res.send({ message: "Successful" });
+    }
+  });
+});
 
 module.exports = router;
