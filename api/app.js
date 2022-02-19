05ce04a7c718e31658ff3c6eed308af5b0e17e6f
Sofia <sofiaq1@gmail.com>
2022-02-19 18:14:42 +0100

    Added hashtags route

--- api/app.js
+++ api/app.js
@@ -3,19 +3,19 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const { Message } = require("./models/message");
-const { User } = require("./models/user");
-
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
+const hashtagsRouter = require("./routes/hashtags");
 
 const app = express();
 const PORT = 9000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/hashtags", hashtagsRouter);
 
 // connects us to the database
 mongoose.connect("mongodb://localhost/twitterClone");
