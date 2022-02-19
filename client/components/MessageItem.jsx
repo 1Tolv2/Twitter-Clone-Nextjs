3a53176c3f4ac30539e4a72ea736059b32ff1ffc
Sofia <sofiaq1@gmail.com>
2022-02-19 12:19:20 +0100

    Message date added

--- client/components/MessageItem.jsx
+++ client/components/MessageItem.jsx
@@ -22,22 +22,37 @@ const StyledContainer = styled.div`
   padding: 10px;
   overflow-x: hidden;
   h3 {
-    margin: 0;
+    margin: 0 10px;
+    width: fit-content;
+  }
+  hr{
+    height: 3px;
+    background-color: #00AFB9;
   }
 `;
 
+const MessageInfoField = styled.div`
+display: flex;
+justify-content: space-between;
+`
+
 export default function MessageItem({data}) {
+  const date = new Date(data.date)
+  const fullDate = `${date.getFullYear()}.${(date.getMonth()+1) < 10 ? "0" + (date.getMonth()+1) : date.getMonth()}.${date.getDate()}`
+  console.log(date)
   return (
-    <StyledSection>
+    <StyledSection key={data._id}>
       <Link href={`/${data.author}`}>
         <StyledImage>
           <img src="profile-svgrepo-com.svg" height="50px" />
         </StyledImage>
       </Link>
       <StyledContainer>
-        <h3>
-          <Link href={`/${data.author}`}>{data.author}</Link>
-        </h3>
+        <MessageInfoField>
+          <h3>
+            <Link href={`/${data.author}`}>{data.author}</Link>
+          </h3><span>{fullDate}</span>
+        </MessageInfoField>
         <hr />
         <p>{data.message}</p>
       </StyledContainer>
