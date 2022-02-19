479316d5daae5be4660727e055dad22e9e66b985
Sofia <sofiaq1@gmail.com>
2022-02-19 18:12:54 +0100

    user data properties added to schema

--- api/models/user.js
+++ api/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  author: { type: String, required: true },
+  user_name: { type: String, required: true },
+  user_password: { type: String, required: true },
 });
 
 const User = mongoose.model("User", userSchema);
