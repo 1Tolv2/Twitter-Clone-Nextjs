6757f15aea6d22d0bddd9fe7636827f3c0c85d53
Sofia <sofiaq1@gmail.com>
2022-02-19 15:05:20 +0100

    Added page/hashtag.js

---
+++ client/pages/hashtag.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import Messageboard from "../components/Messageboard";
+import Layout from "../components/Layout";
+import { API } from "../components/API";
+import MessageMaker from "../components/MessageMaker";
+
+export default function Home({ data }) {
+  const [messageList, setMessageList] = useState(null);
+
+  useEffect(() => {
+    setMessageList(data);
+  }, [data]);
+  return (
+    <Layout>
+      <Messageboard data={messageList}>
+        <MessageMaker />
+      </Messageboard>
+    </Layout>
+  );
+}
+export async function getStaticProps() {
+  const res = await fetch(API, {
+    headers: { "Content-Type": "application/json" },
+  });
+  const data = await res.json();
+
+  return { props: { data } };
+}
