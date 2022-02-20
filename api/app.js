0eea1fc3dfe1537cc44ada8fc4c847797c22094e
Sofia <sofiaq1@gmail.com>
2022-02-20 23:31:50 +0100

    added cors again

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
@@ -8,6 +9,7 @@ const hashtagsRouter = require("./routes/hashtags");
 const app = express();
 const PORT = 9000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", indexRouter);
