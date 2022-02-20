8fae3efa452b10f0c307c8222817109abc7b0be1
Sofia <sofiaq1@gmail.com>
2022-02-20 23:32:26 +0100

    Moved message form code

--- client/components/MessageForm.jsx
+++ client/components/MessageForm.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
+import { API } from './API';
 import HashtagList from "./HashtagList";
 
 
@@ -24,10 +25,11 @@ const StyledTextArea = styled.textarea`
 const StyledButtonContainer = styled.div`
   position: relative;
   float: right;
-  img {
+  .submitImg {
     position: absolute;
     top: 7px;
     left: 15px;
+    height: 25px;
     width: 25px;
     cursor: pointer;
   }
@@ -42,7 +44,44 @@ const StyledButton = styled.button`
   cursor: pointer;
 `;
 
-export default function MessageForm({handleOnSubmit, handleOnClick, handleMessageOnChange, message, placeholder, messageLength, hashtagList}) {
+export default function MessageForm({}) {
+  const [placeholder, setPlaceholder] = useState("");
+  const [message, setMessage] = useState("");
+  const [messageLength, setMessageLength] = useState(0);
+  const [hashtagList, setHashtagList] = useState([]);
+  
+  const placeholderList = ["Lay down the moos", "Moo to your hearts desire"];
+  useEffect(() => {
+    setPlaceholder(placeholderList[Math.floor(Math.random() * 2)]);
+  }, [placeholderList]);
+
+  async function handleOnSubmit(e) {
+    e.preventDefault();
+    console.log("Submit handled");
+    const res = await fetch(API, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ user_name: "Test", message }),
+    });
+    if (!res.ok) {
+      const data = await res.json();
+      console.log(data.errorMessage);
+    }
+    setMessage(""); //Empties the message field after submitting the message
+    setMessageLength(0);
+  }
+  let list = [];
+  function handleMessageOnChange(e) {
+    setMessage(e.target.value);
+    setMessageLength(e.target.value.length);
+      list = [...new Set(e.target.value.match(/#{1}[A-Ö]+(?=\s|$)/gi))];
+        list && setHashtagList(list);
+  }
+
+  function handleOnClick(e) {
+    e.target.style.height = "115px";
+  }
+  
   return (
     <form onSubmit={handleOnSubmit}>
         <StyledTextArea
@@ -56,7 +95,7 @@ export default function MessageForm({handleOnSubmit, handleOnClick, handleMessag
           maxlength="140"
         />
         <StyledButtonContainer onClick={handleOnSubmit}>
-          <img src="./send-paper.svg" />
+          <img className="submitImg" src="./send-paper.svg" />
           <StyledButton></StyledButton>
         </StyledButtonContainer>
         <span className={messageLength > 140 ? "redText" : null}>
--- client/components/MessageMaker.jsx
+++ client/components/MessageMaker.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import { API } from "./API";
+
 import MessageForm from "./MessageForm";
 
 const StyledContainer = styled.div`
@@ -24,48 +24,11 @@ const StyledContainer = styled.div`
 `;
 
 export default function MessageMaker() {
-  const [message, setMessage] = useState("");
-  const [placeholder, setPlaceholder] = useState("");
-  const [messageLength, setMessageLength] = useState(0);
-  const [hashtagList, setHashtagList] = useState([]);
-
-  const placeholderList = ["Lay down the moos", "Moo to your hearts desire"];
-
-  useEffect(() => {
-    setPlaceholder(placeholderList[Math.floor(Math.random() * 2)]);
-  }, []);
-  async function handleOnSubmit(e) {
-    e.preventDefault();
-    console.log("Submit handled");
-    const res = await fetch(API, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ author: "Test", message }),
-    });
-    if (!res.ok) {
-      const data = await res.json();
-      console.log(data.errorMessage);
-    }
-    setMessage(""); //Empties the message field after submitting the message
-    setMessageLength(0);
-  }
-  let list = [];
-  function handleMessageOnChange(e) {
-    setMessage(e.target.value);
-    setMessageLength(e.target.value.length);
-      list = [...new Set(message.match(/#{1}[A-Ö]+(?=\s|$)/gi))];
-      list && setHashtagList(list);
-      console.log(hashtagList);
-  }
-
-  function handleOnClick(e) {
-    e.target.style.height = "115px";
-  }
 
   return (
     <StyledContainer>
       <img src="./Stylized-Cow-Line-Art.svg" />
-      <MessageForm data={handleOnSubmit, handleOnClick, handleMessageOnChange, message, placeholder, messageLength, hashtagList}/>
+      <MessageForm />
     </StyledContainer>
   );
 }
