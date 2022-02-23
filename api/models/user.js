432e21971d2c3c044324eb41af82874c49a2ec5e
1Tolv2 <sofiaq1@gmail.com>
2022-02-23 21:15:39 +0100

    Now encrypts password before save and checks login

--- api/models/user.js
+++ api/models/user.js
@@ -1,9 +1,24 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
-  user_name: { type: String, required: true },
-  user_password: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
+
+// overwrites the password with a encrypted version
+userSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10); // encrypts the password
+  this.password = hash; // overwrites the password
+  next(); // continue to next middleware
+});
+
+userSchema.statics.login = async function (username, password) {
+  const user = await this.findOne({ username });
+  if (user && (await bcrypt.compare(password, user.password))) {
+    return user;
+  } else return null;
+};
 
 const User = mongoose.model("User", userSchema);
 
