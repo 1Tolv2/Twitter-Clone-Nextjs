473ffd3d6cf281bb23c3b9d924924de5f7ea2593
1Tolv2 <sofiaq1@gmail.com>
2022-02-24 21:53:31 +0100

    API now saves usernames with lowercase

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -6,24 +6,30 @@ const { User } = require("../models/user");
 const router = express.Router();
 const JWT_SECRET = "iqokjsjfdhncal546dsggba934a2ab2wer";
 
-// create user
+// POST creates user
 router.post("/users", async (req, res) => {
   const { username, password } = req.body;
-  const user = new User({ username, password });
+  const lowerCaseUsername = username.toLowerCase();
+  const user = new User({ username: lowerCaseUsername, password });
   await user.save();
-  res.json({ username, message: "User created" });
+  res.json({ username: lowerCaseUsername, message: "User created" });
 });
 
-// login user
+// POST logs in user
 router.post("/api-token", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.login(username, password);
+  const lowerCaseUsername = username.toLowerCase();
+  const user = await User.login(lowerCaseUsername, password);
   if (user) {
     const userId = user._id.toString();
-    const token = jwt.sign({ userId, username: username }, JWT_SECRET, {
-      expiresIn: 120,
-      subject: userId,
-    });
+    const token = jwt.sign(
+      { userId, username: lowerCaseUsername },
+      JWT_SECRET,
+      {
+        expiresIn: 120,
+        subject: userId,
+      }
+    );
     res.json({ token });
   } else {
     res.sendStatus(401); // Unauthorized
