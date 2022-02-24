cf4cf5856c91374ab42360a1c6fadca1b7325d97
1Tolv2 <sofiaq1@gmail.com>
2022-02-24 21:54:34 +0100

    added url to /hashtags GET route

--- api/routes/hashtags.js
+++ api/routes/hashtags.js
@@ -2,25 +2,42 @@ const express = require("express");
 const { Message } = require("../models/message");
 const router = express.Router();
 
+const BASE_URL = "http://localhost:9000";
+
+// GET All hashtags and times used
 router.get("/", async (req, res) => {
   const count = await Message.aggregate()
     .unwind("hashtags")
     .group({ _id: "$hashtags", count: { $sum: 1 } })
     .exec();
   const data = count.map((tag) => {
-    return { tag_name: tag._id, count: tag.count };
+    const urlTag = tag._id.replace("#", "%23");
+    return {
+      tag_name: tag._id,
+      count: tag.count,
+      url: `${BASE_URL}/hashtags/${urlTag}`,
+    };
   });
-  res.json(data);
+  res.json({ data });
 });
 
+// GET Hashtag by tag_name
 router.get("/:id", async (req, res) => {
-  const count = await Message.find({ hashtags: req.params.id }).exec();
+  const count = await Message.find({ hashtags: req.params.id })
+    .select("_id")
+    .exec();
+
+  const messages = [];
+  count.map(({ _id }) => {
+    messages.push(_id);
+  });
+
   const data = {
     tag_name: req.params.id,
     count: count.length,
-    messages: count,
+    messages,
   };
-  res.json(data);
+  res.json({ data });
 });
 
 module.exports = router;
