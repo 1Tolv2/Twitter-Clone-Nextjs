ff58b82a5a1c8325a2ade865aafcaad615d3c60a
1Tolv2 <sofiaq1@gmail.com>
2022-02-24 23:50:48 +0100

    added pofile_image and subscribedTo to userSchema

--- api/models/user.js
+++ api/models/user.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs");
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  profile_image: String,
+  subscribedTo: [{ user: { type: String } }],
 });
 
 // overwrites the password with a encrypted version
