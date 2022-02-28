5f63c8e82d2f78c216122537573e79e280c92c77
1Tolv2 <sofiaq1@gmail.com>
2022-02-28 18:55:45 +0100

    Fetches messages specifically if user is logged in

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,12 +4,30 @@ import Layout from "../components/layouts/Layout";
 import { API } from "../components/API";
 import MessageMaker from "../components/molecules/MessageMaker";
 
-export default function Home({ data }) {
+export default function Home() {
   const [messageList, setMessageList] = useState(null);
 
-  useEffect(() => {
+  async function renderMessageList(headers) {
+    console.log(headers);
+    const res = await fetch(`${API}/messages`, headers);
+    const { data } = await res.json();
     setMessageList(data);
-  }, [data]);
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem("Token");
+    let headers = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    if (token) {
+      headers.headers["Authorization"] = `Bearer ${token}`;
+      renderMessageList(headers);
+    } else {
+      renderMessageList(headers);
+    }
+  }, []);
   return (
     <Layout>
       <Messageboard data={messageList}>
@@ -17,13 +35,4 @@ export default function Home({ data }) {
       </Messageboard>
     </Layout>
   );
-}
-export async function getStaticProps() {
-  const res = await fetch(API + "/messages", {
-    headers: { "Content-Type": "application/json" },
-  });
-  const { data } = await res.json();
-  console.log(data);
-
-  return { props: { data } };
 }
