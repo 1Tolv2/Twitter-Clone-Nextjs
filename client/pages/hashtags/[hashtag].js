f1da8a7b2953b609c2146feeb0e2335add8124c6
1Tolv2 <sofiaq1@gmail.com>
2022-02-28 23:23:00 +0100

    Renders specifik tag messages

--- client/pages/hashtags/[hashtag].js
+++ client/pages/hashtags/[hashtag].js
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 import Messageboard from "../../components/molecules/Messageboard";
 import Layout from "../../components/layouts/Layout";
 import { API } from "../../components/API";
 import MessageMaker from "../../components/molecules/MessageMaker";
 
-export default function Home() {
+export default function Home({ data }) {
   const [messageList, setMessageList] = useState(null);
+  const router = useRouter();
 
   useEffect(() => {
-    async () => {
-      const res = await fetch(API, {
-        headers: { "Content-Type": "application/json" },
+    const { hashtag } = router.query;
+    if (hashtag && data) {
+      const hashtagData = data.find((item) => {
+        return item.tag_name == hashtag;
       });
-      const data = await res.json();
-      setMessageList(data);
-    };
-  }, []);
+      setMessageList(hashtagData.messages);
+    }
+  }, [data]);
   return (
     <Layout>
       <Messageboard data={messageList}>
@@ -24,11 +26,22 @@ export default function Home() {
     </Layout>
   );
 }
-// export async function getStaticProps() {
-//   const res = await fetch(API, {
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   const data = await res.json();
+export async function getStaticProps() {
+  const res = await fetch(`${API}/hashtags`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  const { data } = await res.json();
+  return { props: { data } };
+}
 
-//   return { props: { data } };
-// }
+export async function getStaticPaths() {
+  const res = await fetch(`${API}/hashtags`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  const { data } = await res.json();
+
+  const paths = data.map((item) => {
+    return { params: { hashtag: item.tag_name.replace("#", "%23") } };
+  });
+  return { paths, fallback: true };
+}
