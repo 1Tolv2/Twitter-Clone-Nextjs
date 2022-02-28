1909efbf61b7b033e313756013f38719fa31442f
1Tolv2 <sofiaq1@gmail.com>
2022-02-28 13:32:26 +0100

    GSSPaths and GSSProps now functional for /[user]

--- client/pages/[user]/index.js
+++ client/pages/[user]/index.js
@@ -1,60 +1,62 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { API } from "../../components/API";
-import Layout from "../../components/layouts/Layout";
-import Messageboard from "../../components/molecules/Messageboard";
+import Layout from "../../components/Layouts/Layout";
+import Messageboard from "../../components/Molecules/Messageboard";
 
-export default function User() {
+export default function User({ userData }) {
   const router = useRouter();
   const [userMessageList, setUserMessageList] = useState(null);
 
   useEffect(() => {
-    // setUserMessageList(data);
-    // console.log(userMessageList);
-    const fetchData = async () => {
-      const res = await fetch(API, {
-        headers: { "Content-Type": "application/json" },
-      });
-      const data = await res.json();
-      console.log(data);
-      const user = router.query;
-
-      console.log(user.user);
-      const messageList = data.filter((message) => {
-        return message.author === user.user;
-      });
-
-      return setUserMessageList(messageList);
-    };
-
-    fetchData();
-  }, [router.query]);
+    setUserMessageList(userData.data);
+  }, [userData]);
   return (
     <Layout>
+      Hej
       <Messageboard data={userMessageList}></Messageboard>
     </Layout>
   );
 }
 
-// export async function getStaticProps() {
-//   const res = await fetch(`${API}/users`, {
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   const data = await res.json();
+export async function getStaticProps() {
+  const userRes = await fetch(`${API}/users`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const userData = await userRes.json();
+  // console.log("USERDATA:", userData.data);
+  const messageRes = await fetch(`${API}/messages`, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const messageData = await messageRes.json();
+  // console.log("MESSAGEDATA:", messageData.data);
+  userData.data.map((user) => {
+    user.totalMessageList = [];
+    messageData.data.forEach((item) => {
+      // console.log("ITEM:", item);
+      // console.log("USER:", user);
+      return item.username === user.username
+        ? user.totalMessageList.push(item)
+        : null;
+    });
+  });
 
-//   return { props: { data } };
-// }
+  return { props: { userData } };
+}
 
-// export async function getStaticPaths() {
-//   const res = await fetch(`${API}/users`, {
-//     headers: { "Content-Type": "application/json" },
-//   });
-//   const data = await res.json();
-//   const paths = data.map((user) => {
-//     return {
-//       params: { id: user.user_name },
-//     };
-//   });
-//   console.log(paths);
-//   return { paths, fallback: false };
-// }
+export async function getStaticPaths() {
+  const res = await fetch(`${API}/users`, {
+    headers: { "Content-Type": "application/json" },
+  });
+  const { data } = await res.json();
+  const paths = data.map((user) => {
+    return {
+      params: { user: user.username },
+    };
+  });
+  return { paths, fallback: false };
+}
