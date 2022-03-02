2f4aa6a7b777849a7f5fe04f05e4ff47e70fc296
1Tolv2 <sofiaq1@gmail.com>
2022-03-02 15:02:49 +0100

    Added user settings

--- api/models/user.js
+++ api/models/user.js
@@ -3,6 +3,9 @@ const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  firstname: { type: String, required: false },
+  lastname: { type: String, required: false },
+  email: { type: String, required: false },
   password: { type: String, required: true },
   subscribedTo: [{ type: String }],
   subscribers: [{ type: String }],
