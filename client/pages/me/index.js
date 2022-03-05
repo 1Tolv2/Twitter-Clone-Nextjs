6e5f948eff13c268ec95394f42acf2dd7af13af6
1Tolv2 <sofiaq1@gmail.com>
2022-03-06 00:12:31 +0100

    Added content to /me page

---
+++ client/pages/me/index.js
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import { API } from "../../components/API";
+import LayoutUser from "../../components/layouts/LayoutUser";
+import Messageboard from "../../components/molecules/Messageboard";
+import Button from "../../components/atoms/Button";
+
+const StyledContainer = styled.div`
+  display: grid;
+  grid-template-columns: 100px auto auto;
+  gap: 20px;
+  background-color: white;
+  width: 100%;
+  height: fit-content;
+  background-color: white;
+  border-radius: 5px;
+  padding: 10px;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const InfoContainer = styled.div`
+  margin: 10px 0;
+  span {
+    margin: 0 5px;
+  }
+`;
+
+const StyledHeader = styled.h2`
+  margin: 0 10px;
+`;
+
+export default function index() {
+  const router = useRouter();
+
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("Token");
+    if (token) {
+      fetch(`${API}/users/me`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then(({ data }) => setUserData(data));
+    }
+  }, []);
+
+  function handleOnClick() {
+    router.push("/me/settings");
+  }
+
+  return (
+    <LayoutUser>
+      {userData && (
+        <>
+          <StyledContainer>
+            <div>
+              <img src={`${API}/${userData.user.image}`} />
+            </div>
+            <div>
+              {console.log(userData.user.subscribedTo)}
+              <StyledHeader>{userData.user.username}</StyledHeader>
+              {userData.user.settings.name && <li>{userData.user.name}</li>}
+              {userData.user.settings.email && <li>{userData.user.email}</li>}
+              <InfoContainer>
+                <span>Followers: {userData.user.subscribers.length}</span>
+                <span>Following: {userData.user.subscribedTo.length}</span>
+              </InfoContainer>
+            </div>
+            <div>
+              <Button width="100" handleOnClick={handleOnClick}>
+                Settings
+              </Button>
+            </div>
+          </StyledContainer>
+          <Messageboard data={userData.messageList} />
+        </>
+      )}
+    </LayoutUser>
+  );
+}
