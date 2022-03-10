4f3e17fc1ad942bd8de20f4a3e4406a1e26f0df1
1Tolv2 <sofiaq1@gmail.com>
2022-03-11 00:16:17 +0100

    Added likes/dislikes and comments to message schem

--- api/models/message.js
+++ api/models/message.js
@@ -4,6 +4,9 @@ const messageSchema = new mongoose.Schema({
   username: { type: String, required: true, lowercase: true },
   message: { type: String, required: true, maxLength: 140 },
   hashtags: { type: Array, required: false },
+  likes: { type: Number, default: 0 },
+  dislikes: { type: Number, default: 0 },
+  comments: { type: Array, required: false },
   published: { type: Date, default: Date.now, required: true },
 });
 
