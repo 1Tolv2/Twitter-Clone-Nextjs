cdb464a2e40131e8004aa82faf7952f88c19453b
1Tolv2 <sofiaq1@gmail.com>
2022-03-12 00:49:26 +0100

    Added UserContext

--- client2/pages/index.js
+++ client2/pages/index.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState, createContext } from "react";
 import MainLayout from "../components/layouts/mainLayout";
+import { getUserList, getUserData } from "../components/API";
 import BottomNavigationBar from "../components/organisms/BottomNavBar/index";
 import Header from "../components/organisms/Header.jsx";
+import MessageBoard from "../components/organisms/MessageBoard";
+
+const UserContext = createContext();
 
 export default function Home() {
+  const [userList, setUserList] = useState(null);
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    getUserList(setUserList);
+    getUserData(setUserData);
+  }, []);
   return (
     <>
       <Header></Header>
-      <MainLayout>
-        <BottomNavigationBar />
-      </MainLayout>
+      <UserContext.Provider value={{ userData, setUserData }}>
+        <MainLayout>
+          {userList && <MessageBoard userList={userList} />}
+          <BottomNavigationBar />
+        </MainLayout>
+      </UserContext.Provider>
     </>
   );
 }
+
+export { UserContext };
