242a9656d8cd14360abdb50b742da37919d4feae
1Tolv2 <sofiaq1@gmail.com>
2022-03-12 13:29:21 +0100

    Added header links

--- client2/components/organisms/Header.jsx/index.jsx
+++ client2/components/organisms/Header.jsx/index.jsx
@@ -1,8 +1,23 @@
-import * as s from './styles'
-import * as t from '../../atoms/typography/Headings'
+import React from "react";
+import Link from "next/link";
+import * as s from "./styles";
+import * as t from "../../atoms/typography/Headings";
 
 export default function Header() {
   return (
-    <s.Container><t.H1>Twooter</t.H1></s.Container>
-  )
+    <s.Container>
+      <s.Wrapper>
+        <t.H1>Twooter</t.H1>
+        <s.Navigation>
+          <ul>
+            <li>
+              <Link href="/">
+                <s.Icons src="/home-black-svgrepo-com.svg" />
+              </Link>
+            </li>
+          </ul>
+        </s.Navigation>
+      </s.Wrapper>
+    </s.Container>
+  );
 }
--- client2/components/organisms/Header.jsx/styles.jsx
+++ client2/components/organisms/Header.jsx/styles.jsx
@@ -1,9 +1,48 @@
 import styled from "styled-components";
 
 const Container = styled.div`
-width: 100%;
-background-color: white;
-h1{ margin: 0;}
-` 
+  width: 100%;
+  background-color: white;
 
-export {Container}
+
+`;
+
+const Wrapper = styled.div`
+width:100%;
+max-width: 860px;
+margin: auto;
+  h1 {
+    margin-bottom: 9px;
+  }
+  @media (min-width: 1025px) {
+    display: grid;
+    grid-template-columns: 540px 300px;
+    justify-content: space-around;
+    justify-items: center;
+    align-items: center;
+  }
+`
+
+const Navigation = styled.div`
+  display: none;
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+  @media (min-width: 1025px) {
+    display: block;
+    width: 150px;
+    ul {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+    }
+  }
+`;
+
+const Icons = styled.img`
+  height: 30px;
+  margin-top: 1px;
+`;
+export { Container, Wrapper, Navigation, Icons };
