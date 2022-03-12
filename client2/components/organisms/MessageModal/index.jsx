1b184a6496f9c241ffc9aed40faf2c509cc2114f
1Tolv2 <sofiaq1@gmail.com>
2022-03-12 18:17:37 +0100

    Post message feature functional

--- client2/components/organisms/MessageModal/index.jsx
+++ client2/components/organisms/MessageModal/index.jsx
@@ -1,28 +1,71 @@
-import React,{useState} from 'react'
-import AnimatedAddButton from '../../molecules/AnimatedAddButton'
-import * as s from './styles'
+import React, { useState } from "react";
+import AnimatedAddButton from "../../molecules/AnimatedAddButton";
+import {postMessage} from '../../API'
+import * as s from "./styles";
 
 export default function MessageModal() {
   const [modal, setModal] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-    const [iconState, setIconState] = useState("close")
+  const [iconState, setIconState] = useState("close");
+  const [message, setMessage] = useState("");
+  const [messageLength, setMessageLength] = useState(0);
+  const [hashtagList, setHashtagList] = useState([]);
 
-    function toggleIcon() {
-        setModal(!modal);
-        // loginForm ? setIconState("close") : setIconState("open");
-        // setErrorMessage(null)
-      }
+  function toggleIcon() {
+    console.log(modal);
+    setModal(!modal);
+    modal ? setIconState("close") : setIconState("open");
+    // setErrorMessage(null)
+  }
+  function toggleOffModal() {
+    setModal(false);
+    setIconState("close")
+  }
+
+  let list = [];
+  function handleMessageOnChange(e) {
+    setMessage(e.target.value);
+    setMessageLength(e.target.value.length);
+    list = [...new Set(e.target.value.match(/#{1}[A-Ö]+/gi))];
+    list && setHashtagList(list);
+  }
+
+  function handleOnClick(e) {
+    e.preventDefault()
+    const token = localStorage.getItem("Token")
+    const payload = {message}
+    postMessage(token, payload)
+  }
   return (
-    <s.Container>
-        <AnimatedAddButton data={{toggleIcon, iconState}} position={{top: "80vh", left: "10vw"}}></AnimatedAddButton>
-    {modal && (<><s.MessageContainer>
-      <form>
-        <textarea placeholder='Write here...'></textarea>
-        <button><img src="/send-svgrepo-com.svg"/></button>
-      </form>
-    </s.MessageContainer>
-    <s.BlackOutContainer></s.BlackOutContainer></>)}
-
-    </s.Container>
-  )
+    <>
+      <AnimatedAddButton phone
+        data={{ toggleIcon, iconState }}
+        position={{ top: "80vh", left: "10vw" }}
+      ></AnimatedAddButton>
+      {modal && (
+        <s.Container>
+          <s.MessageContainer>
+            <form>
+              <textarea
+                type="text"
+                name="message"
+                value={message}
+                required
+                onChange={handleMessageOnChange}
+                placeholder="Write here..."
+                // maxlength="140"
+              ></textarea>
+              <span className={messageLength > 140 ? "redText" : null}>
+                {messageLength}
+              </span>
+              <button onClick={handleOnClick}>
+                <img src="/send-svgrepo-com.svg" />
+              </button>
+            </form>
+          </s.MessageContainer>
+          <s.BlackOutContainer onClick={toggleOffModal}></s.BlackOutContainer>
+        </s.Container>
+      )}
+    </>
+  );
 }
