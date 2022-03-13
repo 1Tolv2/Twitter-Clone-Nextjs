db467706e8cbb138e3788d8e10e70e8f9ba4a77d
1Tolv2 <sofiaq1@gmail.com>
2022-03-13 19:39:03 +0100

    Added Added side widgets

---
+++ client2/components/molecules/SideWidget/index.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { API } from "../../API";
+import * as s from "./styles";
+
+export default function SideWidget({ title, type }) {
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    if (type === "hashtags") {
+      fetch(`${API}/hashtags?limit=5`, {
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then(({ data }) => setList(data));
+    } else if (type === "users") {
+      fetch(`${API}/users`, {
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((res) => res.json())
+        .then(({ data }) => {
+          let list = [];
+
+          for (let i = 0; i < 5; i++) {
+            const index = Math.floor(Math.random() * data.length);
+            list.push(data[index]);
+            data.splice(index, 1);
+          }
+
+          setList(list);
+        });
+    }
+  }, []);
+
+  function upperCaseName(name) {
+    const modifiedName = name.replace(name[0], name[0].toUpperCase());
+    return modifiedName;
+  }
+
+  return (
+    <>
+      {list && (
+        <s.Container>
+          <h3>{title}</h3>
+          <div>
+            {type === "hashtags" && (
+              <ul>
+                {list &&
+                  list.map((tag, index) => (
+                    <li key={index}>
+                      <Link
+                        href={`/hashtags/${tag.tag_name.replace("#", "%23")}`}
+                      >
+                        <a>{tag.tag_name}</a>
+                      </Link>
+                      <i> - {tag.count}</i>
+                    </li>
+                  ))}
+              </ul>
+            )}
+            {type === "users" && (
+              <ul>
+                {list.map((user, index) => (
+                  <li key={index}>
+                    <Link href={`/${user.username}`}>
+                      <a>{upperCaseName(user.username)}</a>
+                    </Link>
+                    {user.settings.name ? (
+                      <i>{` - ${user.firstname} ${user.lastname}`}</i>
+                    ) : null}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        </s.Container>
+      )}
+    </>
+  );
+}
---
+++ client2/components/molecules/SideWidget/styles.jsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: fit-content;
+  background-color: white;
+  margin-bottom: 10px;
+  border-radius: 5px;
+  padding: 3px;
+  background-image: linear-gradient(120deg, #ff98cb, #b3fff0);
+  hr {
+    border: none;
+    height: 1px;
+    background-color: lightgrey;
+  }
+  h3 {
+    font-size: 1.4em;
+    margin: 10px 5px;
+    color: white;
+    text-transform: uppercase;
+  }
+
+  div {
+    background-color: white;
+    padding: 15px 10px;
+    border-radius: 5px;
+    width: 100%;
+    height: 100%;
+    ul {
+      padding-left: 9px;
+      font-size: 1.1em;
+      a {
+        color: #6600ff;
+        font-weight: 600;
+      }
+    }
+  }
+`;
+
+export {Container}
