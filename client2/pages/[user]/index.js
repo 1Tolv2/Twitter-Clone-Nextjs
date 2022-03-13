3985dce87c47b15d2844d79752fdbe5e7e3043dd
1Tolv2 <sofiaq1@gmail.com>
2022-03-13 12:48:27 +0100

    Added functionality on user pages

--- client2/pages/[user]/index.js
+++ client2/pages/[user]/index.js
@@ -1,5 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { getUserList } from "../../components/API";
+import MainLayout from "../../components/layouts/mainLayout";
+import Header from "../../components/organisms/Header.jsx";
+import MessageBoard from "../../components/organisms/MessageBoard";
+import SideSection from "../../components/organisms/SideSection";
+import EditModal from "../../components/organisms/EditModal";
+import ProfileModule from "../../components/organisms/ProfileModule";
+import BottomNavigationBar from "../../components/organisms/BottomNavBar";
 
 export default function index() {
-  return <div>index</div>;
+  const router = useRouter();
+  const [userList, setUserList] = useState(null);
+  const [editModal, setEditModal] = useState(false);
+
+  useEffect(() => {
+    if (router.query.user) {
+      getUserList(setUserList);
+    }
+  }, [router.query.user]);
+  return (
+    <>
+      <Header></Header>
+      <MainLayout>
+        {editModal && (
+          <EditModal modal={{ editModal, setEditModal }}></EditModal>
+        )}
+        {userList && (
+          <MessageBoard userList={userList} modal={{ editModal, setEditModal }}>
+            <ProfileModule data={userList} />
+          </MessageBoard>
+        )}
+        <SideSection modal={{ editModal, setEditModal }}></SideSection>
+      </MainLayout>
+      <BottomNavigationBar />
+    </>
+  );
 }
