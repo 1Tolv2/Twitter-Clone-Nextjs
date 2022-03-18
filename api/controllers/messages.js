dcd91bde89ac7e0417bf6f6beced19bdd49d9cb4
1Tolv2 <sofiaq1@gmail.com>
2022-03-18 23:27:49 +0100

    Refactored newMessages and added spreading to obj

--- api/controllers/messages.js
+++ api/controllers/messages.js
@@ -2,8 +2,6 @@ const { BASE_URL } = require("../settings");
 const { Message } = require("../models/message");
 const { User } = require("../models/user");
 
-const ObjectId = require("mongodb").ObjectId;
-
 /* Join messages with users and don't get the password
 db.messages.aggregate(
   [
@@ -54,14 +52,7 @@ const getAllMessages = async (req, res) => {
 
   const data = messageList.map((item) => {
     return {
-      _id: item._id,
-      username: item.username,
-      message: item.message,
-      hashtags: item.hashtags,
-      likes: item.likes,
-      dislikes: item.dislikes,
-      comments: item.comments,
-      published: item.published,
+      ...item.toObject(),
       url: `${BASE_URL}/messages/${item._id}`,
       userURL: `${BASE_URL}/users/${item.username}`,
     };
@@ -69,23 +60,22 @@ const getAllMessages = async (req, res) => {
   res.json({ data });
 };
 // Check error handeling
-const newMessage = (req, res, next) => {
+const newMessage = async (req, res, next) => {
   const { message } = req.body;
   const { username } = req.user;
+  typeof message === "undefined" ||
+    (typeof username === "undefined" &&
+      res.json({ error: "Unsuccessful, user and message is required" }, 400));
+
   const hashtags = [...new Set(message.match(/#{1}[A-Ö]+/gi))];
   const messageModel = new Message({
     username,
     message,
     hashtags: hashtags.map((tag) => tag.toLowerCase()),
   });
-  messageModel.save((error) => {
-    if (error) {
-      res.json({ error: "Unsuccessful, user and message is required" }, 400);
-      next(error);
-    } else {
-      res.json({ message: "Message posted successfully" });
-    }
-  });
+
+  await messageModel.save();
+  res.json({ message: "Message posted successfully" });
 };
 
 /* Sort the array of objects gaines by the message merge
@@ -105,40 +95,22 @@ const getUserMessages = async (req, res) => {
   const messageList = await Message.find({ username: req.params.id })
     .sort({ published: -1 })
     .exec();
-  const data = messageList.map(
-    ({
-      _id,
-      username,
-      message,
-      hashtags,
-      likes,
-      dislikes,
-      comments,
-      published,
-    }) => {
-      console.log(likes);
-      return {
-        _id,
-        username,
-        message,
-        hashtags,
-        likes,
-        dislikes,
-        comments,
-        published,
-        userURL: `${BASE_URL}/users/${username}`,
-      };
-    }
-  );
-  res.json(data);
+
+  const data = messageList.map((item) => {
+    return {
+      ...item.toObject(),
+      userURL: `${BASE_URL}/users/${item.username}`,
+    };
+  });
+  console.log(data);
+  res.json({ data });
 };
+
 const deleteMessage = async (req, res) => {
   const message = await Message.deleteOne({ _id: req.params.id }).exec();
-  if (message.deletedCount === 0) {
-    res.json({ success: "Message deleted" });
-  } else {
-    res.json({ error: "Message not found." }, 404);
-  }
+  message.deletedCount === 0
+    ? res.json({ success: "Message deleted" })
+    : res.json({ error: "Message not found." }, 404);
 };
 
 const likeMessage = async (req, res) => {
@@ -161,7 +133,6 @@ const likeMessage = async (req, res) => {
   const updatedMessage = await Message.findById({
     _id,
   });
-  console.log("UPDATED", updatedMessage);
 
   res.json({ updatedMessage });
 };
