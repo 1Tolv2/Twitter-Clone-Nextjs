293300737a627f044818ad3620d296ba987b1326
1Tolv2 <sofiaq1@gmail.com>
2022-03-18 21:25:59 +0100

    Refactored getAllHashtags

--- api/controllers/hashtags.js
+++ api/controllers/hashtags.js
@@ -3,8 +3,6 @@ const { BASE_URL } = require("../settings");
 
 // Needs to be reworked
 const getAllHashtags = async (req, res) => {
-  const limit = req.query.limit;
-  console.log(typeof limit);
   const messageList = await Message.find({
     $match: { hashtags: { $not: { $size: 0 } } },
   }).exec();
@@ -29,25 +27,19 @@ const getAllHashtags = async (req, res) => {
   }
 
   const data = count.map((tag) => {
-    const messages = [];
-
-    messageList.forEach((item) => {
-      if (item.hashtags.includes(tag._id)) {
-        messages.push(item);
-      }
-    });
-
-    const urlTag = tag._id.replace("#", "%23");
-
+    const messages = messageList.filter((item) =>
+      item.hashtags.includes(tag._id)
+    );
     return {
       tag_name: tag._id,
       count: tag.count,
       messages,
-      url: `${BASE_URL}/hashtags/${urlTag}`,
+      url: `${BASE_URL}/hashtags/${tag._id.replace("#", "%23")}`,
     };
   });
   res.json({ data });
 };
+
 const getHashtag = async (req, res) => {
   const messageList = await Message.find({ hashtags: req.params.id }).exec();
   res.json({
